5f31784d67693c635b9f92acd8e736d322fb0ce7
High23 <warenasb2@gmail.com>
2023-05-23 00:38:36 -0700

    Added event listener for user input

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const container = document.querySelector("#container");
 
-function createGrid(){
-    for (let i = 0; i < 16; i++){
+function createGrid(height, width){
+    for (let i = 0; i < height; i++){
         const row = document.createElement("div");
         row.classList.toggle("grid-row");
         container.appendChild(row);
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < width; j++){
             const column = document.createElement("span");
             column.classList.toggle("grid-col");
             row.appendChild(column);
@@ -13,6 +13,11 @@ function createGrid(){
     }
 }
 
-createGrid();
-
+const button = document.querySelector('#createGrid'); 
+button.addEventListener('click', () => {
+    height = document.querySelector('#height').value;
+    width = document.querySelector('#width').value;
+    if (height > 100 || width > 100 || height < 1 || width < 1) return;
+    createGrid(height, width);
+});
 
